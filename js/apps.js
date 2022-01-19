68d17959bb1e07a3ddd05a9dc1744af0b350497b
Harvey Francois <har.francois@gmail.com>
2022-01-18 22:36:04 -0800

    updated and running

--- js/apps.js
+++ js/apps.js
@@ -1,18 +1,18 @@
 'use strict';
 
-let container = document.getElementById('select');
-let resultList = document.getElementById('result');
-let submit = document.getElementById('button');
+const container = document.getElementById('select');
+const submit = document.getElementById('button');
 
 let image1 = document.getElementById('img1');
 let image2 = document.getElementById('img2');
 let image3 = document.getElementById('img3');
 
 let itemArray = [];
+let index = [];
 let counter = 0;
 let maxCount = 25;
 
-function Item(name){
+function Item(name) {
   this.views = 0;
   this.clicks = 0;
   this.name = name;
@@ -20,18 +20,64 @@ function Item(name){
   itemArray.push(this);
 }
 
-function randomNum() {
-  let nArray = [];
-  let item1 = nArray[0];
-  let item2 = nArray[1];
-  let item3 = nArray[2];
-  for( let i=0;i<3;i++){
-    nArray.push(Math.floor(Math.random() * itemArray.length));
-  }
-  return nArray;
+function getRandom() {
+  return Math.floor(Math.random() * itemArray.length);
 }
-randomNum();
 
+function renderItem() {
+  while (index.length < 6) {
+    let number = getRandom();
+    if (!index.includes(number)) {
+      index.push(number);
+    }
+  }
+  let item1 = index.shift();
+  let item2 = index.shift();
+  let item3 = index.shift();
+
+  image1.src = itemArray[item1].src;
+  image1.alt = itemArray[item1].name;
+  image2.src = itemArray[item2].src;
+  image2.alt = itemArray[item2].name;
+  image3.src = itemArray[item3].src;
+  image3.alt = itemArray[item3].name;
+
+  itemArray[item1].views++;
+  itemArray[item2].views++;
+  itemArray[item3].views++;
+
+}
+function handelClicks(event) {
+  counter++;
+  let itemClicked = event.target.alt;
+  for (let i = 0; i < itemArray.length; i++) {
+    if (itemClicked === itemArray[i].name) {
+      itemArray[i].clicks++;
+      break;
+    }
+  }
+  if (counter === maxCount) {
+    container.removeEventListener('click', handelClicks);
+    submit.className = 'clicks-allowed';
+    submit.addEventListener('click', buttonClick);
+  }
+  renderItem();
+}
+function buttonClick(event) {
+  if (counter === maxCount) {
+    renderResult();
+  }
+}
+function renderResult() {
+  let ul = document.querySelector('ul');
+  for (let i = 0; i < itemArray.length; i++) {
+    let message = `${itemArray[i].name} had ${itemArray[i].views} views and was clicked on ${itemArray[i].clicks} times.`;
+    let li = document.createElement('li');
+    li.textContent = message;
+    ul.appendChild(li);
+  }
+
+}
 new Item('bag');
 new Item('banana');
 new Item('bathroom');
@@ -52,6 +98,6 @@ new Item('unicorn');
 new Item('water-can');
 new Item('wine-glass');
 
-image1.src = itemArray[item1].src;
-image2.src = nArray[1].src;
-image3.src = nArray[3].src;
+renderItem();
+
+container.addEventListener('click', handelClicks);
